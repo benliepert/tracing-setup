a70edc3f26977291a923556c0d07e4f3235b55c3
Ben Liepert <ben.liepert@gmail.com>
2025-06-19 13:17:53 -0400

    Collapse mode specific options into configs contained in relevant enum variants

--- src/tracing_setup.rs
+++ src/tracing_setup.rs
@@ -1,3 +1,5 @@
+#[cfg(feature = "jaeger")]
+use crate::jaeger::jaeger_impl::JaegerCfg;
 use chrono::Local;
 use std::fs::File;
 use tracing::subscriber::set_global_default;
@@ -6,39 +8,47 @@ use tracing_subscriber::{
     fmt::{self, format::FmtSpan},
     EnvFilter, FmtSubscriber,
 };
+
 pub const DEFAULT_SPAN_EVENTS: FmtSpan = FmtSpan::CLOSE;
 
-#[derive(Default, Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 /// Controls how tracing is configured at a high level
 pub enum TracingMode {
     /// Log to the console only
-    #[default]
-    Console,
+    Console(ConsoleCfg),
     /// Log to a file only
-    File,
+    File(FileCfg),
     /// Log to both the console and a file
-    ConsoleAndFile,
+    ConsoleAndFile(ConsoleCfg, FileCfg),
     /// Log to a live Jaeger instance. This automatically logs to the console as well
     #[cfg(feature = "jaeger")]
-    JaegerLive,
+    JaegerLive(JaegerCfg),
 }
 
-/// Holds information about how tracing should be configured
-#[derive(Debug)]
-pub struct TracingConfig {
-    pub tracing_mode: TracingMode,
+impl Default for TracingMode {
+    fn default() -> Self {
+        TracingMode::Console(ConsoleCfg::default())
+    }
+}
 
-    /// Environment filter for tracing.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ConsoleCfg {
+    /// ANSI colors in the console
     ///
-    /// If None, uses the default env var with [from_default_env](tracing_subscriber::EnvFilter::from_default_env).
-    /// See [EnvFilter]
-    pub env_filter: Option<String>,
+    /// See [`SubscriberBuilder::with_ansi`](tracing_subscriber::fmt::SubscriberBuilder::with_ansi)
+    pub ansi_console: bool,
+}
 
-    /// Use JSON formatting for logs. This applies to all tracing modes except Jaeger.
-    pub json: bool,
+impl Default for ConsoleCfg {
+    fn default() -> Self {
+        ConsoleCfg { ansi_console: true }
+    }
+}
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct FileCfg {
     /// When writing to a file, this is the directory to write to.
-    pub log_dir: String,
+    pub log_dir: &'static str,
 
     /// ANSI colors in the file
     ///
@@ -49,83 +59,78 @@ pub struct TracingConfig {
     ///
     /// See [`NonBlockingBuilder::lossy`](tracing_appender::non_blocking::NonBlockingBuilder::lossy)
     pub lossy_file: bool,
-
-    /// ANSI colors in the console
-    ///
-    /// See [`SubscriberBuilder::with_ansi`](tracing_subscriber::fmt::SubscriberBuilder::with_ansi)
-    pub ansi_console: bool,
-
-    /// The hostname of the jaeger instance, if applicable
-    #[cfg(feature = "jaeger")]
-    pub jaeger_hostname: String,
 }
 
-impl Default for TracingConfig {
+impl Default for FileCfg {
     fn default() -> Self {
-        TracingConfig {
-            tracing_mode: TracingMode::Console,
-            env_filter: None,
-            json: false,
-            log_dir: "./logs".to_string(),
+        FileCfg {
+            log_dir: "./logs",
             ansi_file: false,
             lossy_file: true,
-            ansi_console: true,
-            #[cfg(feature = "jaeger")]
-            jaeger_hostname: "localhost".to_string(),
         }
     }
+}
+
+/// Holds information about how tracing should be configured
+#[derive(Debug, Default)]
+pub struct TracingConfig {
+    pub tracing_mode: TracingMode,
+
+    /// Environment filter for tracing. This controls the log levels and modules that are logged.
+    ///
+    /// If None, uses the default env var with [from_default_env](tracing_subscriber::EnvFilter::from_default_env).
+    /// See [EnvFilter]
+    pub env_filter: Option<String>,
+
+    /// Use JSON formatting. This applies to all tracing modes except Jaeger.
+    pub json: bool,
 }
 
 /// Initialize the tracing system based on a config
 ///
 /// Returns an option to a WorkerGuard, which will flush all pending logs when dropped.
-/// This is specifically for writing to a file. When `config.log_to_file` is false, this will
-/// return None.
+/// This is specifically for writing to a file. For ['crate::TracingMode::Console`] and [`TracingMode::JaegerLive`], this always returns None.
 pub fn init_tracing(config: TracingConfig) -> Option<WorkerGuard> {
     // this separation is necessary because adding layers changes the type of the subscriber,
     // so it's impossible to genericize and make this cleaner^
     match config.tracing_mode {
-        TracingMode::Console => {
-            configure_console_logging(config);
+        TracingMode::Console(console_cfg) => {
+            configure_console_logging(&console_cfg, config.env_filter, config.json);
             None
         }
-        TracingMode::File => configure_file_logging(config),
-        TracingMode::ConsoleAndFile => configure_combined_logging(config),
+        TracingMode::File(file_cfg) => {
+            configure_file_logging(&file_cfg, config.env_filter, config.json)
+        }
+        TracingMode::ConsoleAndFile(console_cfg, file_cfg) => {
+            configure_combined_logging(&console_cfg, &file_cfg, config.env_filter, config.json)
+        }
         #[cfg(feature = "jaeger")]
-        TracingMode::JaegerLive => {
+        TracingMode::JaegerLive(jaeger_cfg) => {
             use crate::jaeger::jaeger_impl::*;
-            wait_for_jaeger(&config.jaeger_hostname); // block until jaeger is running
+            wait_for_jaeger(&jaeger_cfg.jaeger_hostname); // block until jaeger is running
             println!("Initializing tracing for live streaming to Jaeger. Make sure you start the Jaeger Docker container.");
-            init_jaeger(config);
-
+            init_jaeger(&jaeger_cfg, &config.env_filter);
             None
         }
-    }
-}
-
-/// Return an environment filter based on the provided config.
-/// If config.env_filter is None, the default filter will be used
-/// See [`EnvFilter::from_default_env`](tracing_subscriber::EnvFilter::from_default_env)
-pub fn env_filter(config: &TracingConfig) -> String {
-    if let Some(filter) = &config.env_filter {
-        filter.clone()
-    } else {
-        EnvFilter::from_default_env().to_string()
     }
 }
 
 /// Only file logging will be configured
-fn configure_file_logging(config: TracingConfig) -> Option<WorkerGuard> {
-    let (non_blocking, guard) = file_writer(&config);
+fn configure_file_logging(
+    config: &FileCfg,
+    env_filter: Option<String>,
+    json: bool,
+) -> Option<WorkerGuard> {
+    let (non_blocking, guard) = file_writer(config);
 
     let subscriber_builder = FmtSubscriber::builder()
-        .with_env_filter(env_filter(&config))
+        .with_env_filter(get_env_filter(&env_filter))
         .with_span_events(DEFAULT_SPAN_EVENTS)
         .with_ansi(config.ansi_file)
         .with_writer(non_blocking);
 
     // since adding json formatting changes the type, some code needs to be duplicated
-    if config.json {
+    if json {
         let subscriber = subscriber_builder.json().finish();
         set_global_default(subscriber).expect("Failed to set global default");
     } else {
@@ -137,15 +142,15 @@ fn configure_file_logging(config: TracingConfig) -> Option<WorkerGuard> {
 }
 
 /// Only console logging will be configured
-fn configure_console_logging(config: TracingConfig) {
+fn configure_console_logging(config: &ConsoleCfg, env_filter: Option<String>, json: bool) {
     // let format = fmt::format().json();
     let subscriber_builder = FmtSubscriber::builder()
-        .with_env_filter(env_filter(&config))
+        .with_env_filter(get_env_filter(&env_filter))
         .with_span_events(DEFAULT_SPAN_EVENTS)
         .with_ansi(config.ansi_console);
 
     // since adding json formatting changes the type, some code needs to be duplicated
-    if config.json {
+    if json {
         let subscriber = subscriber_builder.json().finish();
         set_global_default(subscriber).expect("Failed to set global default");
     } else {
@@ -156,51 +161,69 @@ fn configure_console_logging(config: TracingConfig) {
 
 /// Both console and file logging will be configured
 // reward for whoever can make this cleaner
-fn configure_combined_logging(config: TracingConfig) -> Option<WorkerGuard> {
+fn configure_combined_logging(
+    console_cfg: &ConsoleCfg,
+    file_cfg: &FileCfg,
+    env_filter: Option<String>,
+    json: bool,
+) -> Option<WorkerGuard> {
     use tracing_subscriber::prelude::*;
+
     // File writer setup
-    let (non_blocking, guard) = file_writer(&config);
+    let (non_blocking, guard) = file_writer(file_cfg);
+    let env_filter = EnvFilter::new(get_env_filter(&env_filter));
 
     // due to some complexities in the type system, this code is duplicated
     // the only difference is that when layers are added to the subscriber, if config.json
     // json formatting is used.
-    if config.json {
+    if json {
         let stdout_layer = fmt::layer()
             .with_writer(std::io::stdout)
             .with_span_events(DEFAULT_SPAN_EVENTS)
-            .with_ansi(config.ansi_console);
+            .with_ansi(console_cfg.ansi_console);
         let file_layer = fmt::layer()
             .with_writer(non_blocking)
             .with_span_events(DEFAULT_SPAN_EVENTS)
-            .with_ansi(config.ansi_file);
+            .with_ansi(file_cfg.ansi_file);
         let subscriber = tracing_subscriber::registry()
             .with(stdout_layer.json())
             .with(file_layer.json())
-            .with(EnvFilter::new(env_filter(&config)));
+            .with(env_filter);
         set_global_default(subscriber).expect("Failed to set global default");
     } else {
         let stdout_layer = fmt::layer()
             .with_writer(std::io::stdout)
             .with_span_events(DEFAULT_SPAN_EVENTS)
-            .with_ansi(config.ansi_console);
+            .with_ansi(console_cfg.ansi_console);
         let file_layer = fmt::layer()
             .with_writer(non_blocking)
             .with_span_events(DEFAULT_SPAN_EVENTS)
-            .with_ansi(config.ansi_file);
+            .with_ansi(file_cfg.ansi_file);
         let subscriber = tracing_subscriber::registry()
             .with(stdout_layer)
             .with(file_layer)
-            .with(EnvFilter::new(env_filter(&config)));
+            .with(env_filter);
         set_global_default(subscriber).expect("Failed to set global default");
     }
     Some(guard)
 }
 
+/// Return an environment filter based on the provided config.
+/// If config.env_filter is None, the default filter will be used
+/// See [`EnvFilter::from_default_env`](tracing_subscriber::EnvFilter::from_default_env)
+pub(crate) fn get_env_filter(filter: &Option<String>) -> String {
+    if let Some(filter) = &filter {
+        filter.clone()
+    } else {
+        EnvFilter::from_default_env().to_string()
+    }
+}
+
 /// Create a file writer based on a tracing config
 ///
 /// The file writer will log tracing information to a file
-fn file_writer(config: &TracingConfig) -> (NonBlocking, WorkerGuard) {
-    let filename = create_log_filename(&config.log_dir);
+fn file_writer(config: &FileCfg) -> (NonBlocking, WorkerGuard) {
+    let filename = create_log_filename(config.log_dir);
     create_parent_directory(&filename).expect("Failed to create parent log directory");
     let file_writer = File::create(&filename).expect("Failed to create log file");
     NonBlockingBuilder::default()
@@ -214,8 +237,7 @@ fn create_log_filename(log_dir: &str) -> String {
     format!("{}/log_{}.txt", log_dir, now.format("%m-%d-%Y_%H-%M-%S"))
 }
 
-/// Given a path, create the parent directory if it doesn't exist.
-/// Otherwise, do nothing.
+/// Given a path, create the parent directory if it doesn't exist. Otherwise, do nothing.
 fn create_parent_directory(path: &str) -> std::io::Result<()> {
     let path = std::path::Path::new(path);
     if let Some(parent) = path.parent() {
